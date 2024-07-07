35878aa53ae2f46b7024994424de06dcde7c77df
delToros <nikitademkin@gmail.com>
2024-07-07 11:58:25 +0300

    Initial commit

---
+++ slug_game.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "slug_game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, slug_game, "slug_game" );
